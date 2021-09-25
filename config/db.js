10abbeaf1d02ad439e071a6b1d5f3a8b7805d453
I'm Unix <kaslamkaleng024@gmail.com>
2021-09-25 17:21:40 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const DB_URI = 'mongodb://mongo:6SGCbw4YjG8KxLMNGaCr@containers-us-west-14.railway.app:5792'
+const DB_URI = 'mongodb://mongo:NXDTquE7XyoH1f6Kme7H@containers-us-west-15.railway.app:6802'
 
 mongoose.connect(DB_URI,{useNewUrlParser:true, useUnifiedTopology:true})
 
