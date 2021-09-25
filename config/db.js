6e960a96baa15bab8a258c0c5d3956765f4bf6d6
I'm Unix <kaslamkaleng024@gmail.com>
2021-09-25 10:07:18 +0000

    initial commit

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const DB_URI  = mongodb://mongo:6SGCbw4YjG8KxLMNGaCr@containers-us-west-14.railway.app:5792'
+
+mongoose.connect(DB_URI,{useNewUrlParser:true, useUnifiedTopology:true})
+
+const connection = mongoose.connection
+
+module.exports = connection
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+// Database config
+const connection = require('./config/db')
+connection.once('open', () => console.log('DB Connected'))
+connection.on('error', () => console.log('Error'))
+
+// Routes Config
+app.use(express.json({
+    extended: false
+})) //parse incoming request body in JSON format.
+app.use('/', require('./routes/redirect'))
+app.use('/api/url', require('./routes/url'))
+
+//Listen for incoming requests
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`server started, listening PORT ${PORT}`))
